f54f3d251f14a777a820a65582938a9f7cecff46
chengdashia <1847085602@qq.com>
2022-07-29 10:14:10 +0800

    【Springboot】ip2region 补充

--- ip2region/src/main/java/com/example/ip2region/controller/IpController.java
+++ ip2region/src/main/java/com/example/ip2region/controller/IpController.java
@@ -16,6 +16,114 @@ import org.springframework.web.bind.annotation.RestController;
 public class IpController {
     private final Ip2regionSearcher ip2regionSearcher;
 
+    ///**
+    // * ip 位置 搜索
+    // *
+    // * @param ip ip
+    // * @return 位置
+    // */
+    //@Nullable
+    //IpInfo memorySearch(long ip);
+    //
+    ///**
+    // * ip 位置 搜索
+    // *
+    // * @param ip ip
+    // * @return 位置
+    // */
+    //@Nullable
+    //IpInfo memorySearch(String ip);
+    //
+    ///**
+    // * ip 位置 搜索
+    // *
+    // * @param ptr ptr
+    // * @return 位置
+    // */
+    //@Nullable
+    //IpInfo getByIndexPtr(long ptr);
+    //
+    ///**
+    // * ip 位置 搜索
+    // *
+    // * @param ip ip
+    // * @return 位置
+    // */
+    //@Nullable
+    //IpInfo btreeSearch(long ip);
+    //
+    ///**
+    // * ip 位置 搜索
+    // *
+    // * @param ip ip
+    // * @return 位置
+    // */
+    //@Nullable
+    //IpInfo btreeSearch(String ip);
+    //
+    ///**
+    // * ip 位置 搜索
+    // *
+    // * @param ip ip
+    // * @return 位置
+    // */
+    //@Nullable
+    //IpInfo binarySearch(long ip);
+    //
+    ///**
+    // * ip 位置 搜索
+    // *
+    // * @param ip ip
+    // * @return 位置
+    // */
+    //@Nullable
+    //IpInfo binarySearch(String ip);
+    //
+    //
+    ///**
+    // * 直接获取地址信息，完整的地址，例如：北京朝阳区xxx
+    // *
+    // * @param ip ip
+    // * @return 地址
+    // */
+    //@Nullable
+    //default String getAddress(long ip) {
+    //    return getInfo(ip, IpInfo::getAddress);
+    //}
+    //
+    ///**
+    // * 直接获取地址信息，完整的地址，例如：北京朝阳区xxx
+    // *
+    // * @param ip ip
+    // * @return 地址
+    // */
+    //@Nullable
+    //default String getAddress(String ip) {
+    //    return getInfo(ip, IpInfo::getAddress);
+    //}
+    //
+    ///**
+    // * 获取地址信息包含 isp
+    // *
+    // * @param ip ip
+    // * @return 地址
+    // */
+    //@Nullable
+    //default String getAddressAndIsp(long ip) {
+    //    return getInfo(ip, IpInfo::getAddressAndIsp);
+    //}
+    //
+    ///**
+    // * 获取地址信息包含 isp
+    // *
+    // * @param ip ip
+    // * @return 地址
+    // */
+    //@Nullable
+    //default String getAddressAndIsp(String ip) {
+    //    return getInfo(ip, IpInfo::getAddressAndIsp);
+    //}
+
     /**
      * 获取地理位置信息
      * @param ip  ip地址
@@ -23,7 +131,6 @@ public class IpController {
      */
     @GetMapping("test")
     public String test(String ip) {
-        // getAddressAndIsp  中国 河南 洛阳 电信 多了一个运营商
         log.info(""+ip2regionSearcher.getAddressAndIsp(ip));
         return ip2regionSearcher.getAddress(ip);
     }
